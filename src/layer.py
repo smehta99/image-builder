314fac7b0160a9af459e6a5f6477e955199ece13
Shivam Mehta <smehta@lanl.gov>
2025-09-04 08:47:25 -0600

    add repodir creation back to layer.py
    Signed-off-by: Shivam Mehta <smehta@lanl.gov>

--- src/layer.py
+++ src/layer.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import pathmod
 import sys
 import os
 # written modules
@@ -54,7 +55,7 @@ class Layer:
             repo_dest = "/etc/zypp/repos.d"
         elif package_manager == "dnf":
             repo_dest = os.path.expanduser("~/.pkg_repos/yum.repos.d")
-            os.makedirs(repo_dest, exist_ok=True)
+            os.makedirs(os.path.join(mname, pathmod.sep_strip(repo_dest)), exist_ok=True)
         else:
             self.logger.error("unsupported package manager")
 
